4c78a18b01fe7aa20b9014436ff91574d8e59788
JanjyTapYT <81481526+JanjyTapYT@users.noreply.github.com>
2022-07-15 12:38:49 +0200

    fixed config

---
+++ config/stoneclane.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    token: process.env.token,
+    prefix: "s!",
+    owners: ["679407120743137300", "530832812274614303"],
+    footer: "Bot made with ❤️ and ☕ by Stoneclane Development"
+}
+  
