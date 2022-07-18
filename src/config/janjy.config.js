4f21ed85be9cc0e52f5a8dea77866a9ff3d65363
huh? ! <81481526+JanjyTapYT@users.noreply.github.com>
2022-07-18 23:48:54 +0200

    Update janjy.config.js

--- src/config/janjy.config.js
+++ src/config/janjy.config.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client({ intents: 32767 });
 
 module.exports = {
-    token: "OTk1Mzk3MjYzMTMwMTEyMDgx.GESS9E.nZApjARH1nMNnFmA9F7V13FotLDXOhrqq-lhTo",
-    mongo: "mongodb+srv://janjyfy:hi@cluster0.vur7v.mongodb.net/?retryWrites=true&w=majority",
+    token: "TOKEN GOES HERE!",
+    mongo: "MONGO GOES HERE!",
     owners: ["679407120743137300", "530832812274614303"],
     footer: "Bot made with ❤️ and ☕ by Stoneclane Development",
 
