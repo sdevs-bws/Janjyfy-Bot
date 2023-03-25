cfa3387d9f2320df18fe30a70e5cf91fce027b2b
JanjyTapYT <janjytapyt@sdevs.org>
2023-03-25 08:17:08 +0100

    Update c4.js

--- checks/c4.js
+++ checks/c4.js
@@ -16,3 +16,36 @@ module.exports = async (client) => {
   console.log("[ℹ️] If the emojis don't load, please check if I have the 'USE_EXTERNAL_EMOJIS' permission in the server!");
 
 };
+module.exports = (client) => {
+
+  const guildId = "892311236878934016";
+
+  const guild = client.guilds.fetch(guildId).catch(() => {
+    console.log("[❌] I'm not in the C4 Emoji Server so the Emojis wont load! Add me to the server by sending a message to JanjyTapYT#0001! || https://discord.gg/TbuHeKFUnJ");
+  })
+
+  if (guild) {
+
+    console.log("[✅] I'm in the C4 Emoji server!");
+
+  }
+
+  console.log("[ℹ️] If the emojis don't load, please check if I have the 'USE_EXTERNAL_EMOJIS' permission in the server!");
+
+}; module.exports = (client) => {
+
+  const guildId = "892311236878934016";
+
+  const guild = client.guilds.fetch(guildId).catch(() => {
+    console.log("[❌] I'm not in the C4 Emoji Server so the Emojis wont load! Add me to the server by sending a message to JanjyTapYT#0001! || https://discord.gg/TbuHeKFUnJ");
+  })
+
+  if (guild) {
+
+    console.log("[✅] I'm in the C4 Emoji server!");
+
+  }
+
+  console.log("[ℹ️] If the emojis don't load, please check if I have the 'USE_EXTERNAL_EMOJIS' permission in the server!");
+
+}; 
