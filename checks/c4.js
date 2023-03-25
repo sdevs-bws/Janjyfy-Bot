7bac28458aab1f0e85e682a88da9249c543f84a9
JanjyTapYT <janjytapyt@sdevs.org>
2023-03-25 07:58:01 +0100

    Update c4.js

--- checks/c4.js
+++ checks/c4.js
@@ -1,11 +1,19 @@
-module.exports = (client) => {
-    const guild = client.guilds.cache.get("892311236878934016");
+module.exports = async (client) => {
 
-    if (guild) {
-        console.log("[✅] Im in the " + guild.name + " !");
-    } else {
-        console.log("[❌] I'm not in the C4 Emoji Server so the Emojis wont load! Add me to the server by sending a message to JanjyTapYT#0001! || https://discord.gg/TbuHeKFUnJ");
-    }
+  const guildId = "892311236878934016";
 
-    console.log("[ℹ️] If the Emojis dont load, please check if i have the 'USE_EXTERNAL_EMOJIS' permission in the server!");
+  const guild = await client.guilds.fetch(guildId);
+
+  if (guild.members.cache.has(client.user.id)) {
+
+    console.log("[✅] I'm a member of the " + guild.name + " server!");
+
+  } else {
+
+    console.log("[❌] I'm not a member of the " + guild.name + " server!");
+
+  }
+
+  console.log("[ℹ️] If the emojis don't load, please check if I have the 'USE_EXTERNAL_EMOJIS' permission in the server!");
+
 };
